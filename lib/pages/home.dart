682d0f524b42d12bfecdf3f6f9ecef654b00cb0f
dgvdilshan@students.nsbm.ac.lk <vimukthidilshan23933@gmail.com>
2025-05-05 23:57:27 +0530

    creating the home page in app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/widget/widget_support.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,19 +9,109 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool icecream = false, pizza = false, salad = false, burger = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         margin: EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Hello Vimu,",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Hello Vimu,", style: AppWidget.boldTextFeildStyle()),
+                Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text("Delicious Food,", style: AppWidget.HeadlineTextFeildStyle()),
+            Text("Discover and Get Great Food ,",
+                style: AppWidget.LightTextFeildStyle()),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    icecream = true;
+                    pizza = false;
+                    burger = false;
+                    salad = false;
+                    setState(() {});
+                  },
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: icecream ? Colors.black : Colors.white,
+                          borderRadius: BorderRadius.circular(8)),
+                      padding: EdgeInsets.all(8),
+                      child: Image.asset(
+                        "images/ice-cream.jpeg",
+                        height: 50.0,
+                        width: 50.0,
+                        fit: BoxFit.cover,
+                        color: icecream ? Colors.white : Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Image.asset(
+                      "images/pizza.jpeg",
+                      height: 50.0,
+                      width: 50.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Image.asset(
+                      "images/burger.jpg",
+                      height: 50.0,
+                      width: 50.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Image.asset(
+                      "images/salad.jpg",
+                      height: 50.0,
+                      width: 50.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              ],
             )
           ],
         ),
