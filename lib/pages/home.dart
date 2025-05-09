cecf2fbdb9b7b0ff86d977c53fcce00f7ef81069
DgvDilshan <dgvdilshan@students.nsbm.ac.lk>
2025-05-10 02:10:44 +0530

    add the gesturedetector of ui

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -72,45 +72,84 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                 ),
-                Material(
-                  elevation: 5.0,
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    padding: EdgeInsets.all(8),
-                    child: Image.asset(
-                      "images/pizza.png",
-                      height: 50.0,
-                      width: 50.0,
-                      fit: BoxFit.cover,
+                GestureDetector(
+                  onTap: () {
+                    icecream = false;
+                    pizza = true;
+                    burger = false;
+                    salad = false;
+                    setState(() {});
+                  },
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: pizza ? Colors.black : Colors.white,
+                          borderRadius: BorderRadius.circular(8)),
+                      padding: EdgeInsets.all(8),
+                      child: Image.asset(
+                        "images/pizza.png",
+                        height: 50.0,
+                        width: 50.0,
+                        fit: BoxFit.cover,
+                        color: pizza ? Colors.white : Colors.black,
+                      ),
                     ),
                   ),
                 ),
-                Material(
-                  elevation: 5.0,
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    padding: EdgeInsets.all(8),
-                    child: Image.asset(
-                      "images/burger.png",
-                      height: 50.0,
-                      width: 50.0,
-                      fit: BoxFit.cover,
+                GestureDetector(
+                  onTap: () {
+                    icecream = false;
+                    pizza = false;
+                    burger = true;
+                    salad = false;
+                    setState(() {});
+                  },
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: burger ? Colors.black : Colors.white,
+                          borderRadius: BorderRadius.circular(8)),
+                      padding: EdgeInsets.all(8),
+                      child: Image.asset(
+                        "images/burger.png",
+                        height: 50.0,
+                        width: 50.0,
+                        fit: BoxFit.cover,
+                        color: burger ? Colors.white : Colors.black,
+                      ),
                     ),
                   ),
                 ),
-                Material(
-                  elevation: 5.0,
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    padding: EdgeInsets.all(8),
-                    child: Image.asset(
-                      "images/salad.png",
-                      height: 50.0,
-                      width: 50.0,
-                      fit: BoxFit.cover,
+                GestureDetector(
+                  onTap: () {
+                    icecream = false;
+                    pizza = false;
+                    burger = false;
+                    salad = true;
+                    setState(() {});
+                  },
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: salad ? Colors.black : Colors.white,
+                          borderRadius: BorderRadius.circular(8)),
+                      padding: EdgeInsets.all(8),
+                      child: Image.asset(
+                        "images/salad.png",
+                        height: 50.0,
+                        width: 50.0,
+                        fit: BoxFit.cover,
+                        color: salad ? Colors.white : Colors.black,
+                      ),
                     ),
                   ),
-                )
+                ),
               ],
             )
           ],
